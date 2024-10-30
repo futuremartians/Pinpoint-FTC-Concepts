3aa9e22b6ab374578df48784fbad05a5f30b8d1f
isleofmant <sriharshap@live.com>
2024-10-29 20:16:35 -0700

    Added gyroturntoangle method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -22,11 +22,13 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
@@ -41,6 +43,7 @@ public class AutoTest extends LinearOpMode {
 
     GoBildaPinpointDriver odo;
     DcMotor frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor;
+    IMU imu;
 
     @Override
     public void runOpMode() {
@@ -58,10 +61,20 @@ public class AutoTest extends LinearOpMode {
         frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        // Retrieve the IMU from the hardware map
+        imu = hardwareMap.get(IMU.class, "imu");
+        // Adjust the orientation parameters to match your robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
+
         // Wait for the game to start (driver presses START)
         waitForStart();
         resetRuntime();
 
+        gyroTurnToAngle(90);
         driveToPos(new Pose2D(DistanceUnit.CM, 20, 20, AngleUnit.DEGREES, 0));
         sleep(10000);
     }
@@ -93,6 +106,35 @@ public class AutoTest extends LinearOpMode {
             telemetry.update();
         }
 
+        frontLeftMotor.setPower(0);
+        backLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+        backRightMotor.setPower(0);
+    }
+
+    public void gyroTurnToAngle(double turnAngle) {
+        double error, currentHeadingAngle, driveMotorsPower;
+        imu.resetYaw();
+
+        error = turnAngle;
+
+        while (opModeIsActive() && ((error > 1) || (error < -1))) {
+            driveMotorsPower = error / 200;
+
+            if ((driveMotorsPower < 0.2) && (driveMotorsPower > 0)) {
+                driveMotorsPower = 0.2;
+            } else if ((driveMotorsPower > -0.2) && (driveMotorsPower < 0)) {
+                driveMotorsPower = -0.2;
+            }
+
+            // Positive power causes left turn
+            frontLeftMotor.setPower(-driveMotorsPower);
+            backLeftMotor.setPower(-driveMotorsPower);
+            frontRightMotor.setPower(driveMotorsPower);
+            backRightMotor.setPower(driveMotorsPower);
+
+            currentHeadingAngle = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+        }
         frontLeftMotor.setPower(0);
         backLeftMotor.setPower(0);
         frontRightMotor.setPower(0);
